882cdf4a6c72dc6bf461ff76a10e74dbe7c68fea
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-29 18:02:00 -0500

    classes renamed

---
+++ Extension_Method/Genre.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_Method
+{
+    public enum Genre
+    {
+        Action,Thriller, Scifi, Drama, Romance, Horror, Adventure, Documentary, Western, Fiction, Animation
+    }
+}
---
+++ Extension_Method/Movies.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_Method
+{
+    public class Movies
+    {
+        private double Rating;
+        private string MovieTitle;
+        private string Producer;
+        private string Director;
+        private double BoxOffice;
+        private DateTime ReleaseDate;
+
+        private Genre genre { get; set; }
+
+
+        public Movies(double rating, string movieTitle, string producer, string director, double boxOffice,DateTime releaseDate, Genre genre)
+        {
+            Rating = rating;
+            MovieTitle = movieTitle;
+            Producer = producer;
+            Director =director;
+            BoxOffice = boxOffice;
+            ReleaseDate = releaseDate;
+
+        }
+        public double rating
+        {
+            get => Rating;set => Rating = value;
+        }
+        public string producer
+        {
+            get => producer; set =>Producer = value;
+        }
+        public string director
+        {
+            get => Director; set => Director = value;
+        }
+        public double boxOffice
+        {
+            get => BoxOffice; set => BoxOffice = value;
+        }
+        public DateTime releaseDate
+        {
+            get => ReleaseDate; set => ReleaseDate = value;
+        }
+    }
+    
+}
