df00e317e4b87422948a7d804d63f97fcc61c3f4
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-02-29 17:36:51 -0500

    created employee class with properties and enum

---
+++ Extension_Method/JobTitle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Extension_Method
+{
+    public enum JobTitle
+    {
+        ProjectManager,SystemAnalyst,BusinessAnalyst,SoftwareDeveloper,SoftwareTester,
+    }
+}
